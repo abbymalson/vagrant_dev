4e0593d9c2c5e65fe71742bdd138e3b598b19759
abby malson (harry potter box) <amalson@weedmaps.com>
2017-12-14 17:06:33 -0700

    today's changes

--- code/am_jobrunner/actions/database_settings.php
+++ code/am_jobrunner/actions/database_settings.php
@@ -1,7 +1,25 @@
 <?php
 // /usr/bin/php /vagrant/code/am_jobrunner/cli_job_runner/cli_job_runner.php
 
-$server="localhost";
-$dbname="am_jobrunner";
-$user="root";
-$pass="WM41Discovery";
+    $dbSettings = array();
+switch (gethostname()) {
+  case "harry-potter":
+    $server="localhost";
+    $dbname="am_jobrunner";
+    $user="root";
+    $pass="WM41Discovery";
+    $dbSettings['server'] = $server;
+    $dbSettings['dbname'] = $dbname;
+    $dbSettings['user'] = $user;
+    $dbSettings['pass'] = $pass;
+  default:
+    $server="localhost";
+    $dbname="am_jobrunner";
+    $user="root";
+    $pass="easypeasy1";
+    $dbSettings['server'] = $server;
+    $dbSettings['dbname'] = $dbname;
+    $dbSettings['user'] = $user;
+    $dbSettings['pass'] = $pass;
+    break;
+}
